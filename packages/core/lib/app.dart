53106ddca18a50d6331a2b34174c5a77146b90dc
Muhammad Azri Fatihah Susanto <Muhammad.azri.f.s@gmail.com>
2024-05-26 13:27:40 +0700

    fix(interceptors):fix-refresh-token-&-navigate-to-login

--- packages/core/lib/app.dart
+++ packages/core/lib/app.dart
@@ -3,15 +3,18 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const ScreenUtilInit(
+    return ScreenUtilInit(
       minTextAdapt: true,
-      designSize: Size(375, 812),
+      designSize: const Size(375, 812),
       child: MaterialApp(
+        navigatorKey: navigatorKey,
         debugShowCheckedModeBanner: kDebugMode,
         onGenerateRoute: AppRouter.onGenerateRoute,
       ),
